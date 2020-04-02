e18a538771a3db41f7243b560d3847c4c1043ff2
halimtullah <60643867+halimtullah@users.noreply.github.com>
2020-04-02 08:04:29 +0300

    update akan.js
    add male and female names

--- akan.js
+++ akan.js
@@ -6,7 +6,7 @@ function readData() {
     var female = document.getElementById("female").checked; //return true if female button selected and false if unselected
     var days = document.getElementById["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] //array for days
     var maleNames = document.getElementById["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yao", "Kofi", "Kwame"] //array for maleNames
-    var femaleName = document.getElementById["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"] //array for femaleNames
+    var femaleNames = document.getElementById["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"] //array for femaleNames
     var birthDate = new Date(mm + '/' + dd + '/' + yy);
 
     var daysOfTheWeek = birthDate.getDate(); //calculation of days of the week. 
