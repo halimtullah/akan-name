2e2484092b735a376443ac647471f4aef8eb96dd
halimtullah <60643867+halimtullah@users.noreply.github.com>
2020-04-03 11:48:37 +0300

    update akan.js
    add all

--- akan.js
+++ akan.js
@@ -9,8 +9,35 @@ function readData() {
     var femaleNames = document.getElementById["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"] //array for femaleNames
     var birthDate = new Date(mm + '/' + dd + '/' + yy);
 
-    var daysOfTheWeek = birthDate.getDate(); //calculation of days of the week. 
+    var daysOfTheWeek = birthDate.getDate(); //calculation of days of the week.
+    if (dd <=0 || dd>31) {// ensures the user put a date and validates the dates.
+            document.getElementById("error").innerText = "Please enter a valid date" // shows an error message
+            document.getElementById("error")
+    }    else if ((mm === "January" || mm === "March" || mm === "May" || mm === "July" || mm === "August" || mm === "October" || mm === "December") && (dd <= 0 || dd > 30)){
+            document.getElementById("error").innerText = "Please enter a valid date!" 
+            document.getElementById("error")
+    }   else if (mm === "---"){
+            document.getElementById("error").innerText = "Please select a month!" 
+            document.getElementById("error")
+    }   else if (yy <= 0 || yy > 2020){
+            document.getElementById("error").innerText = "Please Enter a valid year!"
+            document.getElementById("error")
+    }   else if ((mm === "February") && (dd <= 0 || dd > 29) && (0 == yy % 4)){
+            document.getElementById("error").innerText = "Please enter a valid date!"
+            document.getElementById("error")
+    }   else if ((mm === "February") && (dd <= 0 || dd > 28) && (0 != yy % 4)){
+            document.getElementById("error").innerText = "Please enter a valid date!"
+            document.getElementById("error")
+    }   else if (male === true){
+            document.getElementById("result").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + maleNames[dayOfTheWeek] + "!"
+            document.getElementById("result").style
+    }   else if (female === true){
+            document.getElementById("result").innerHTML = "You were born on a " + days[dayOfTheWeek] + ".\n" + "Your Akan name is " + femaleNames[dayOfTheWeek] + "!"
+            document.getElementById("result")
+    }    else if ((female === false) && (male === false)){
+            document.getElementById("error").innerText = "Please select gender!"
+            document.getElementById("error")
+    }
+} 
 
 
-
-}
